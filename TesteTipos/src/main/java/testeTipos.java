b4830209abf88302942c6061f59fb80d857fc67b
Calazansh <henriquecalazans.96@gmail.com>
2022-05-14 14:58:17 -0300

    Exemplos de conversão de variaveis tipo int para String e vice e versa

---
+++ TesteTipos/src/main/java/testeTipos.java
@@ -0,0 +1,11 @@
+public class testeTipos {
+    public static void main(String[] args) {
+      /*  int idade = 30;
+        String valor = Integer.toString(idade);
+        System.out.println("A minha idade é "+ valor);
+    */
+        String valor = "30";
+        int idade = Integer.parseInt(valor);
+        System.out.println(idade);
+    }
+}
