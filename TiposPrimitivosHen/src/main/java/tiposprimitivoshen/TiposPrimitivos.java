8fe04f2fcdfb6589e199d772bc2a6a2fc199bea3
Calazansh <henriquecalazans.96@gmail.com>
2022-05-14 14:25:39 -0300

    Tipos primitivos com Prinf para Saída de Dados

---
+++ TiposPrimitivosHen/src/main/java/tiposprimitivoshen/TiposPrimitivos.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposprimitivoshen;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author em Henrique Calazans
+ */
+public class TiposPrimitivos {
+    public static void main (String [] args){
+        Scanner teclado = new Scanner (System.in);
+        System.out.print("Digite o nome do aluno: ");
+        String nome = teclado.nextLine();
+        System.out.print("Digite a nota do aluno: ");
+        float nota = teclado.nextFloat();
+        System.out.format("A nota de %s é %.1f \n", nome, nota);
+        
+        
+        
+    }
+           
+    
+}
