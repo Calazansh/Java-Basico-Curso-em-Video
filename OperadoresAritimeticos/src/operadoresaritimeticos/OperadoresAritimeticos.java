600afd941e7b222c062295ef3bc69f1e8e972596
Calazansh <henriquecalazans.96@gmail.com>
2022-05-14 15:38:29 -0300

    Trabalhando com operadores Aritimeticos da Aula 07

---
+++ OperadoresAritimeticos/src/operadoresaritimeticos/OperadoresAritimeticos.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresaritimeticos;
+
+/**
+ *
+ * @author em
+ */
+public class OperadoresAritimeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      /*int x = 4;
+       x*= 2; 
+       System.out.println(x);*/
+      /*float v = 8.4f;
+      int ar = (int) Math.round(v);
+      System.out.println(ar);*/
+      double ale = Math.random();
+      int n = (int)(0 + ale * (100-0)); 
+      System.out.println(n);
+              
+          
+       
+               
+    }
+    
+}
